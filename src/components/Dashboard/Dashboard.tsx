519be26e48b69842792e3b13b9bb37f3a85bd442
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-05-06 23:14:39 +0400

    feat: add Dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // styles
 import styles from "./Dashboard.module.css";
 
 const Dashboard: React.FC = (): JSX.Element => {
-  return <div>dashboard</div>;
+  const [animate, setAnimate] = useState<boolean>(false);
+
+  useEffect(() => {
+    setAnimate(true);
+  }, []);
+
+  return (
+    <header
+      className={`${styles.dashboardHeaderContainer} ${
+        animate ? styles.animate : ""
+      }`}>
+      <img src={"/images/dashboard/rocket.png"} className={styles.dashboardHeaderRocketImg} alt='rocket' />
+      <div className={styles.dashboardHeader}>
+        <img src={"/images/dashboard/dots.png"} className={styles.dashboardHeaderDotsImg} alt='dots' />
+        <div className={styles.dashboardHeaderTop}>
+          <h1>Hi</h1>
+          <h1>I hope you'll enjoy</h1>
+        </div>
+        <div className={styles.dashboardHeaderBottom}>
+          <h1></h1>
+          <p>this is just a guess...</p>
+        </div>
+        <img src={"/images/dashboard/man.png"} className={styles.dashboardHeaderManImg} alt='man' />
+        <img
+          src={"/images/dashboard/planet.png"}
+          className={styles.dashboardHeaderPlanetImg}
+          alt='planet'
+        />
+      </div>
+      <img src={"/images/dashboard/dotsRight.png"} className={styles.dashboardHeaderDotsRightImg} alt='dots' />
+    </header>
+  );
 };
 
 export default Dashboard;
